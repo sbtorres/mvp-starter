eef47c2974152022d656860f310177d4ca02ad02
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-21 13:54:52 -0700

    Update server route to getPurchases function

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var items = require('../db');
+var db = require('../db');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
+app.get('/purchases', function (req, res) {
+  db.getPurchases(function(err, data) {
     if(err) {
       res.sendStatus(500);
     } else {
