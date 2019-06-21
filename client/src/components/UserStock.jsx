a672edba133d51e94f36c15c254eb4e5daeaa7e7
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-21 14:04:41 -0700

    Modify template to proper naming for application

---
+++ client/src/components/UserStock.jsx
@@ -0,0 +1,9 @@
+import React from './node_modules/react';
+
+const UserStock = (props) => (
+  <div>
+    { props.purchase.description }
+  </div>
+)
+
+export default UserStock;
---
+++ client/src/components/UserStocksList.jsx
@@ -0,0 +1,12 @@
+import React from './node_modules/react';
+import UserStock from './UserStock.jsx';
+
+const UserStocksList = (props) => (
+  <div>
+    <h4> Holdings: </h4>
+    You own { props.purchases.length } stocks.
+    { props.purchases.map(item => <UserStock purchase={purchase}/>)}
+  </div>
+)
+
+export default UserStocksList;
