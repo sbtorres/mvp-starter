99947cd4682be709c5bf931714748a973aa34200
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-22 15:55:33 -0700

    Fix close button not activating, add onChange hanlder to input forms

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -76,7 +76,9 @@ class App extends React.Component {
             {this.state.marketData.length > 1 ? ComparisonData : (<div></div>)}
           </div>
         </div>
-        <button onClick={this.handleStockPurchaseClick}>Add A Stock!</button>
+        <div style={{"display": "flex", "width": "30%", "justifyContent": "center", "padding-top": "10px"}}>
+          <button onClick={this.handleStockPurchaseClick}>Add A Stock!</button>
+        </div>
         <div>
           <StockInputForm hideStockPurchaseModal={this.hideStockPurchaseModal} isVisible={this.state.stockPurchaseModalIsVisible}/>
         </div>
