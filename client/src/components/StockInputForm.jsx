ca088f8e3e507b575c5f20a09fa4ca8e79f36b77
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-22 11:59:20 -0700

    Create new component StockInputForm to handle user input

---
+++ client/src/components/StockInputForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class StockInputForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <div>test</div>
+    );
+  }
+}
+
+export default StockInputForm;
