d7559d71b5e328b7495566a3433aec6a71b5ead5
Scott Torres <scotttorres@Scotts-Air.attlocal.net>
2019-06-23 16:10:04 -0700

    Add total to user stocks

--- client/src/components/UserStocksList.jsx
+++ client/src/components/UserStocksList.jsx
@@ -8,6 +8,7 @@ const UserStocksList = (props) => (
       <h4 className="left-header-col-1">Ticker</h4>
       <h4 className="left-header-col-2">Number of Shares</h4>
       <h4 className="left-header-col-3">Share Price</h4>
+      <h4 className="left-header-col-4">Total</h4>
     </div>
     <div className="user-stocks-list">
       { props.purchases.map(purchase => <UserStock key={purchase.id} purchase={purchase}/>)}
