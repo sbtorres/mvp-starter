0df26afb780420e0539fe3d7382dda17a00d0419
Scott Torres <scott.torres27@gmail.com>
2019-07-07 13:01:20 -0700

    Improve code readability in PortfolioOverview.jsx

--- client/src/components/PortfolioOverview.jsx
+++ client/src/components/PortfolioOverview.jsx
@@ -5,11 +5,26 @@ const PortfolioOverview = (props) => {
     <div>
       <h2 style={{"display": "flex", "justifyContent": "center", "margin": "5px 5px"}}>Portfolio Overview</h2>
       <div className="top-headers">
-        <div className="portfolio-column-1"><strong>Current Net Worth:&nbsp;</strong>{'$' + props.userPortfolio.userCurrentTotal.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
-        <div className="portfolio-column-2"><strong>Total Growth:&nbsp;</strong> {(100 * ((props.userPortfolio.userCurrentTotal - props.userPortfolio.userBaseline) / props.userPortfolio.userBaseline)).toFixed(2) + '%'}</div>
-        <div className="portfolio-column-3"><strong>vs. S&amp;P 500:&nbsp;</strong> {(100 * ((props.userPortfolio.userCurrentTotal - props.userPortfolio.sp500CurrentTotal) / props.userPortfolio.userBaseline)).toFixed(2) + '%'}</div>
-        <div className="portfolio-column-4"><strong>vs. Nasdaq:&nbsp;</strong> {(100 * ((props.userPortfolio.userCurrentTotal - props.userPortfolio.nasdaqCurrentTotal) / props.userPortfolio.userBaseline)).toFixed(2) + '%'}</div>
-        <div className="portfolio-column-5"><strong>vs. Dow Jones:&nbsp;</strong> {(100 * ((props.userPortfolio.userCurrentTotal - props.userPortfolio.dowCurrentTotal) / props.userPortfolio.userBaseline)).toFixed(2) + '%'}</div>
+        <div className="portfolio-column-1">
+          <strong>Current Net Worth:&nbsp;</strong>
+          {'$' + props.userPortfolio.userCurrentTotal.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </div>
+        <div className="portfolio-column-2">
+          <strong>Total Growth:&nbsp;</strong>
+          {(100 * ((props.userPortfolio.userCurrentTotal - props.userPortfolio.userBaseline) / props.userPortfolio.userBaseline)).toFixed(2) + '%'}
+        </div>
+        <div className="portfolio-column-3">
+          <strong>vs. S&amp;P 500:&nbsp;</strong>
+          {(100 * ((props.userPortfolio.userCurrentTotal - props.userPortfolio.sp500CurrentTotal) / props.userPortfolio.userBaseline)).toFixed(2) + '%'}
+        </div>
+        <div className="portfolio-column-4">
+          <strong>vs. Nasdaq:&nbsp;</strong>
+          {(100 * ((props.userPortfolio.userCurrentTotal - props.userPortfolio.nasdaqCurrentTotal) / props.userPortfolio.userBaseline)).toFixed(2) + '%'}
+        </div>
+        <div className="portfolio-column-5">
+          <strong>vs. Dow Jones:&nbsp;</strong>
+          {(100 * ((props.userPortfolio.userCurrentTotal - props.userPortfolio.dowCurrentTotal) / props.userPortfolio.userBaseline)).toFixed(2) + '%'}
+        </div>
       </div>
     </div>
   )
