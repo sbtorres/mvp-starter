5f7d5c5329da16b9ac1e7c89fca700b5026e32d9
Scott Torres <scott.torres27@gmail.com>
2019-07-09 17:56:19 -0700

    Allow purchases visibility to be toggled on click using hooks

--- client/src/components/UserStock.jsx
+++ client/src/components/UserStock.jsx
@@ -1,40 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Purchase from './Purchase.jsx';
 import SummaryComparison from './SummaryComparison.jsx';
 
-const UserStock = (props) => (
-  <div>
-    <div className="stocks-container">
-      <div className="user-stock">
-          <div className="user-stock-ticker">
-            { props.stockSummary.stock_ticker }
-          </div>
-          <div className="user-num-shares">
-            { props.stockSummary.num_of_shares }
-          </div>
-          <div className="purchase-share-price">
-            { '$' + props.stockSummary.avg_share_price.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }
-          </div>
-          <div className="current-share-price">
-            { '$' + props.stockSummary.current_share_price.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }
-          </div>
-          <div className="user-total">
-            { '$' + (props.stockSummary.current_share_price * props.stockSummary.num_of_shares).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-          </div>
-          <div className="stock-percent-gain">
-            {(100 * (props.stockSummary.current_share_price - props.stockSummary.avg_share_price) / props.stockSummary.avg_share_price).toFixed(2) + '%'}
-          </div>
-      </div>
-    <div className="comparison-list">
-      <SummaryComparison marketData={props.marketData} stockSummary={props.stockSummary} />
-    </div>
-    </div>
+const UserStock = (props) => {
+
+  const [arePurchasesVisible, setArePurchasesVisible] = useState(false);
+
+  function handleStockClick(event) {
+    setArePurchasesVisible(!arePurchasesVisible)
+  }
+
+  return (
     <div>
-      { props.stockSummary.individual_purchases.map(purchase => 
-        <Purchase key={purchase.id} marketData={props.marketData} purchase={purchase} stockSummary={props.stockSummary}/> 
-      )}
+      <div className="stocks-container">
+        <div className="user-stock" onClick={handleStockClick}>
+            <div className="user-stock-ticker">
+              { props.stockSummary.stock_ticker }
+            </div>
+            <div className="user-num-shares">
+              { props.stockSummary.num_of_shares }
+            </div>
+            <div className="purchase-share-price">
+              { '$' + props.stockSummary.avg_share_price.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }
+            </div>
+            <div className="current-share-price">
+              { '$' + props.stockSummary.current_share_price.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }
+            </div>
+            <div className="user-total">
+              { '$' + (props.stockSummary.current_share_price * props.stockSummary.num_of_shares).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </div>
+            <div className="stock-percent-gain">
+              {(100 * (props.stockSummary.current_share_price - props.stockSummary.avg_share_price) / props.stockSummary.avg_share_price).toFixed(2) + '%'}
+            </div>
+        </div>
+      <div className="comparison-list">
+        <SummaryComparison marketData={props.marketData} stockSummary={props.stockSummary} />
+      </div>
+      </div>
+        {arePurchasesVisible && 
+            <div>
+              { props.stockSummary.individual_purchases.map(purchase => 
+                <Purchase key={purchase.id} marketData={props.marketData} purchase={purchase} stockSummary={props.stockSummary}/> 
+              )}
+            </div>
+        }
     </div>
-  </div>
-)
+  )
+}
 
 export default UserStock;
