17ae0599ef472b87ad2e1ad4a431a187af40ecc9
Scott Torres <scott.torres27@gmail.com>
2019-07-14 17:46:03 -0700

    Add babel class properties plugin and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['@babel/react', '@babel/env'],
+          plugins: ['@babel/plugin-proposal-class-properties']
         },
       },
     ],
