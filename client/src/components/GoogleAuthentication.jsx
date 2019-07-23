9d57e4275ca34cd78035f5a854500d85859607fa
Scott Torres <scott.torres27@gmail.com>
2019-07-23 15:23:24 -0700

    Add callback function to window.gapi call

--- client/src/components/GoogleAuthentication.jsx
+++ client/src/components/GoogleAuthentication.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CLIENT_ID from '../../config/clientId.js';
 
 class GoogleAuthentication extends React.Component {
   constructor(props) {
@@ -7,7 +8,12 @@ class GoogleAuthentication extends React.Component {
   }
 
   componentDidMount() {
-    window.gapi.load('client:auth2');
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: CLIENT_ID,
+        scope: 'email'
+      })
+    });
   }
 
   render() {
