e30de691a3166abd133ddba080e0215cfb80048d
Scott Torres <scott.torres27@gmail.com>
2019-08-06 21:53:08 -0700

    Set up webpack config to have environment variable for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
+const webpack = require('webpack');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
+const devBuild = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: ['babel-polyfill',`${SRC_DIR}/index.jsx`],
@@ -23,4 +25,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+         CLIENT_ID: JSON.stringify(process.env.CLIENT_ID)
+      }
+    })
+  ]
 };
