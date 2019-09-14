957d601c9f29f36715b6ee05a22d2ff088e5a761
Scott Torres <scott.torres27@gmail.com>
2019-09-14 15:07:46 -0700

    Implement handleSubmit and handleInputChange methods

--- client/src/components/useStockInputForm.js
+++ client/src/components/useStockInputForm.js
@@ -1,7 +1,28 @@
 import React, { useState } from "react";
 
-const useStockInputForm = () => {
-  return null;
+const useStockInputForm = callback => {
+  const [inputs, setInputs] = useState({});
+
+  const handleSubmit = e => {
+    if (e) {
+      e.preventDefault();
+    }
+    callback();
+  };
+
+  const handleInputChange = e => {
+    e.persist();
+    setInputs(inputs => ({
+      ...inputs,
+      [event.target.name]: event.target.value
+    }));
+  };
+
+  return {
+    handleSubmit,
+    handleInputChange,
+    inputs
+  };
 };
 
 export default useStockInputForm;
